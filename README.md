295533fff835c00ba51dac7e6f5feaebc3c7dabb
Jakub Hlavaty <jakub.hlavaty@zf.com>
2016-11-07 13:53:48 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ng-knob-bug
 Bug demonstration for https://github.com/RadMie/ng-knob
+
+The knob value is updated incrementally. Last value shown is _(max + 1)_ and then the knob stops responding. 
+
+Run
+```
+npm start
+```
+and then navigate to http://localhost:8080 in your browser.
---
+++ main.js
@@ -0,0 +1,32 @@
+var module = angular.module('main', ['ui.knob']);
+module.controller('mainController', function($scope, $interval) {
+    $scope.model = {
+        value: 1,
+        options: {
+            readOnly: true,
+            animate: {enabled: true, duration: 1000, ease: 'bounce'},
+            startAngle: -140,
+            endAngle: 140,
+            size: 118,
+            unit: '',
+            trackColor: 'rgb(255,255,255)',
+            barColor: '#005CA8',
+            trackWidth: 10,
+            textColor: '#005CA8',
+            barWidth: 10,
+            fontSize: 41,
+            subText: {
+                enabled: true,
+                text: 'km/h',
+                color: '#005CA8'
+            },
+            min: 0,
+            max: 3,
+            dynamicOptions: true
+        }
+    };
+
+    $interval(function () {
+        $scope.model.value++;
+    }, 2000);
+});
